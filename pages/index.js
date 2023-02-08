f3c3ced70c1f67c0334f8325e849bf3cd0e91d5f
ravitripathi02 <91239605+ravitripathi02@users.noreply.github.com>
2023-02-08 19:57:39 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -188,7 +188,7 @@ export default function Home() {
                       <Image src={code} width="64px" height="64px" alt="/" />
                     </div>
                     <div className="flex flex-col items-center justify-center">
-                      <h3>Firebase</h3>
+                      <h3>Java</h3>
                     </div>
                   </div>
                 </div>
